674e53f2ba68d11d2f26b7a3110c0f20463e9c05
MIGUEL CARDOZO GOMES <miguelcardozogomes@gmail.com>
2026-06-18 22:39:38 -0300

    Testa variaveis  de email

--- src/routes/mensagemRoutes.js
+++ src/routes/mensagemRoutes.js
@@ -4,7 +4,13 @@ import { enviarMensagem } from "../controllers/mensagemController.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "Rota mensagens funcionando!" });
+  res.json({
+    message: "Rota mensagens funcionando!",
+    EMAIL_USER: process.env.EMAIL_USER ? "OK" : "VAZIO",
+    EMAIL_PASS: process.env.EMAIL_PASS ? "OK" : "VAZIO",
+    EMAIL_DESTINO: process.env.EMAIL_DESTINO ? "OK" : "VAZIO",
+    EMAIL_DESTIN0: process.env.EMAIL_DESTIN0 ? "OK" : "VAZIO",
+  });
 });
 
 router.post("/", enviarMensagem);
