7112819aa6039bffbe414ef49aed974ce49e8617
MIGUEL CARDOZO GOMES <miguelcardozogomes@gmail.com>
2026-06-18 21:51:20 -0300

    Ajusta rota de mensagens

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import clinicaRoutes from "./routes/clinicaRoutes.js";
-import { enviarMensagem } from "./controllers/mensagemController.js";
+import mensagemRoutes from "./routes/mensagemRoutes.js";
 
 const app = express();
 
@@ -13,13 +13,8 @@ app.get("/", (req, res) => {
   res.json({ message: "API Vida TEA funcionando!" });
 });
 
-app.get("/mensagens", (req, res) => {
-  res.json({ message: "Rota mensagens funcionando!" });
-});
-
-app.post("/mensagens", enviarMensagem);
-
 app.use("/auth", authRoutes);
 app.use("/clinicas", clinicaRoutes);
+app.use("/mensagens", mensagemRoutes);
 
 export default app;
--- src/routes/mensagemRoutes.js
+++ src/routes/mensagemRoutes.js
@@ -4,9 +4,7 @@ import { enviarMensagem } from "../controllers/mensagemController.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({
-    message: "Rota mensagens funcionando"
-  });
+  res.json({ message: "Rota mensagens funcionando!" });
 });
 
 router.post("/", enviarMensagem);
